8f3c8b1d7afb9c129379b0bb8d48076532ac0805
Furkan Eralp Kahyaoğlu <82308276+kahyaoglou@users.noreply.github.com>
2022-06-05 03:07:54 +0300

    Add files via upload

---
+++ Ucak-Bileti-Rezervasyon-Sistemi/Ucak-Bileti-Rezervasyon-Sistemi/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07.Proje___Uçak_Bileti_Rezervasyon_Sistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Rota: " + comboBox1.Text + " - " + comboBox2.Text + " / Tarih: " +dateTimePicker1.Text + " / Saat: " +maskedTextBox1.Text + " ///// Yolcu Bilgileri ~~ Ad: " + textBox1.Text + " / TC No: " +maskedTextBox2.Text + " / Telefon: "+maskedTextBox3.Text);
+            MessageBox.Show("Rezervasyon Başarılı! ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label9.Text = comboBox2.Text;        //Burada köprü yapmış olduk
+            comboBox2.Text = comboBox1.Text;     //label9 köprüde kullanılan ek eleman oldu
+            comboBox1.Text = label9.Text;        //Fazlalık değer label9'a atandı diyebiliriz. label9 buradaki aracımız.
+        }
+    }
+}
